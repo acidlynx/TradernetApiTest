f9343c6d44c4dd0b828b43c65293efa609beb59c
Oleg Fedyakin <acid.lynx@ya.ru>
2020-09-06 17:20:24 +0300

    chore: init project + pods

---
+++ TradernetApiTest/TradernetApiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TradernetApiTest
+//
+//  Created by User on 06.09.2020.
+//  Copyright © 2020 Binira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
